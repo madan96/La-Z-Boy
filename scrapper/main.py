06bba820e2d12e7a202baccf0ec481571fa7ad9c
Rishabh Madan <rishabhmadan96@gmail.com>
2016-07-12 21:31:12 +0530

    Added IMDb scrapper functionality

--- scrapper/main.py
+++ scrapper/main.py
@@ -1,16 +1,39 @@
 import sys
 import re
-import requests
+import urllib2
 from bs4 import BeautifulSoup
+from mechanize import Browser
+
+'''
+   Currently supports:  #star-movies
+			#sony-max
+			#movies-now
+			#romedy-now
+			#movies-ok
+			#sony-pix
+			#hbo
+			#filmy
+			#star-gold
+'''
 
 web_url = "http://tvinfo.in/"
+base_url = 'http://www.imdb.com/find?q='
+
+def getBSoup(url):
+	req = urllib2.urlopen(url)
+	soup = BeautifulSoup(req.read(),"lxml")
+	return soup
 
 def search_channel(channel):
-	time = []
 	channel_url = web_url + channel + ".html"
-	r = requests.get(channel_url)
-	soup = BeautifulSoup(r.text)
+	soup = getBSoup(channel_url);
+	time = []
+	ratings = []
+	title_search = re.compile('/title/tt\d+')
+
 	movie_name = soup.find_all('p',{"class":"title2"})
+	for i in range(0,len(movie_name)):
+		movie_name[i] = movie_name[i].text
 
 	time1 = soup.find_all('div',{"class":"col-lg-12"})
 	for i in range(1,len(time1)-1,2):
@@ -18,35 +41,35 @@ def search_channel(channel):
 
 	#time = filter(None, time)
 	time = [x for x in time if x != '']
+
 	for i in range(0,len(movie_name)):
-		str1 = "Movie: " + str(movie_name[i]) + "Time: " + str(time[i])
+		movie_search = '+'.join(movie_name[i].split())
+		movie_url = base_url + movie_search + '&s=all'
+		br = Browser()
+		br.open(movie_url)
+		link = br.find_link(url_regex = re.compile(r'/title/tt.*'))
+		res = br.follow_link(link)
+		
+		soup = BeautifulSoup(res.read(),"lxml")	 
+		movie_title = soup.find('title').contents[0]
+		rate = soup.find('span',itemprop='ratingValue')
+		if rate!=None:
+			ratings.append(str(rate.contents[0]))
+		else:
+			ratings.append("-")
+
+ 	for i in range(0,len(movie_name)):
+ 		str1 = "Movie: " + str(movie_name[i]) + "  Time: " + str(time[i])+ "  Rating: " + ratings[i]
 		print(str1)
-	
-	#print(time1[4].text)
-	#for data in time1:
-	#print(data)
-	#	print("------------------")
-	#print(soup.find_all('p',"data-date"))
 
 def main():
 	if(len(sys.argv)>2):
 		channel = str(sys.argv[1] + "-" + sys.argv[2])
 	else:
 		channel = str(sys.argv[1])
-
 	movie_rating = search_channel(channel)
 
 if __name__ == '__main__':
 	main()
 
-#star-movies
-#sony-max
-#movies-now
-#romedy-now
-#movies-ok
-#sony-pix
-#hbo
-#filmy
-#star-gold
-#
-#
+
