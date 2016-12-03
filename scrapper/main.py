8a045e0f04ee5836be1ebb5afa3ed139e54641a5
Swastik Haldar <swastik.haldar@gmail.com>
2016-12-03 11:10:27 +0530

    scrape from tvschedulindia.com successful.But showing duplicate movie names in the final list

--- scrapper/main.py
+++ scrapper/main.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from mechanize import Browser
 from tabulate import tabulate
 import fpdf
+import string
 
 '''
    Currently supports:  #star-movies
@@ -19,6 +20,7 @@ import fpdf
 '''
 
 web_url = "http://tvinfo.in/"
+web_url2= "http://tvscheduleindia.com/channel/"
 base_url = 'http://www.imdb.com/find?q='
 
 #Method to initialize pdf object
@@ -39,7 +41,7 @@ def getBSoup(url):
     return soup
 
 
-def search_channel(channel):
+def search_channel(channel,channel2):
     channel_url = web_url + channel + ".html"
     soup = getBSoup(channel_url)
     time = []
@@ -47,15 +49,53 @@ def search_channel(channel):
     title_search = re.compile('/title/tt\d+')
 
     movie_name = soup.find_all('p', {"class": "title2"})
+
+    channel2_url= web_url2 + channel2
+    soup2 = getBSoup(channel2_url)
+
+    for s in soup2.find_all("strong"):
+        if s.string:
+            s.string.replace_with(s.string.strip())
+
+    movie_name2 = soup2.find_all("strong")
+
+    for i in range(0,len(movie_name2)):
+        movie_name2[i]=movie_name2[i].text
+
     for i in range(0, len(movie_name)):
         movie_name[i] = movie_name[i].text
+    #print movie_name
 
     time1 = soup.find_all('div', {"class": "col-lg-12"})
+
+    for s in soup2.find_all('b',{"class":"from"}):
+        if s.string:
+            s.string.replace_with(s.string.strip())
+
+    for s in soup2.find_all('b',{"class":"to"}):
+        if s.string:
+            s.string.replace_with(s.string.strip())
+
+    time2_from = soup2.find_all('b',{"class":"from"})
+    time2_to = soup2.find_all('b',{"class":"to"})
+    
+    for i in range(0,len(time2_from)):
+        time2_from[i]=time2_from[i].text
+
+    for i in range(0,len(time2_to)):
+        time2_to[i]=time2_to[i].text
+
     for i in range(1, len(time1)-1, 2):
         time.append(time1[i].text[0:13].strip(''))
-
-    # time = filter(None, time)
     time = [x for x in time if x != '']
+
+    for i in range(0,len(movie_name2)):
+        if(movie_name2[i] not in movie_name):
+            movie_name.append(movie_name2[i])
+            time.append(time2_from[i]+"-"+time2_to[i])
+
+    #print time
+    # time = filter(None, time)
 
     for i in range(0, len(movie_name)):
         movie_search = '+'.join(movie_name[i].split())
@@ -90,9 +130,14 @@ def search_channel(channel):
 def main():
     if(len(sys.argv) > 2):
         channel = str(sys.argv[1] + "-" + sys.argv[2])
+        channel2 = str(sys.argv[1]+ "-" +sys.argv[2])
+        channel2 = channel2.title()
     else:
         channel = str(sys.argv[1])
-    movie_rating = search_channel(channel)
+        channel2 = str(sys.argv[1])
+        channel2 = channel2.upper()
+
+    movie_rating = search_channel(channel,channel2)
 
 if __name__ == '__main__':
     main()
